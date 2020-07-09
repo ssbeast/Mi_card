73184fc09cddbc7a37d338358f3f3e5494d00e5b
ssbeast <ssbeast007@gmail.com>
2020-07-09 11:05:59 +0530

    image added in circle avatar

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,15 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Container(
-          height: 100.0,
-          width: 100.0,
-          margin: EdgeInsets.only(left: 30.0),
-          padding: EdgeInsets.all(20.0),
-          color: Colors.white,
-          child: Text("Hello"),
-        )),
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/ssbeast.jpeg'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
