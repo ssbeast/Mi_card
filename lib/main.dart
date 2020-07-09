919f8fbaf46b5a73ca67c39beab844c82c45a0f1
ssbeast <ssbeast007@gmail.com>
2020-07-09 11:18:56 +0530

    font addded

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
               Text(
                 "Saurabh Sahu",
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 40.0,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
