c9493954f419d54da0ec46445409a7d79002fc00
ssbeast <ssbeast007@gmail.com>
2020-07-09 11:08:57 +0530

    styling done

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,14 @@ class MyApp extends StatelessWidget {
                 radius: 50.0,
                 backgroundImage: AssetImage('images/ssbeast.jpeg'),
               ),
+              Text(
+                "Saurabh Sahu",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
             ],
           ),
         ),
